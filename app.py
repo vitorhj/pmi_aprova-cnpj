abdd8796a66d13d4ec3298fe3c2ff9ef68ad3b2a
vitorhj <90215531+vitorhj@users.noreply.github.com>
2023-11-14 15:01:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,7 @@ st.sidebar.button("Limpar", on_click=clear_text)
 
 st.title('Confere processos ALF')
 if (texto_aprova) == '':
-     st.text(str('<<< Copie e cole as informações na barra lateral esquerda.'))
+     st.markdown(str('<<< Copie e cole as informações na barra lateral esquerda.'))
      
 #_____________________________________________________________________________________________________________________
  
